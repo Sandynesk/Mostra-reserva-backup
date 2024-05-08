2a5d01a6846238ac50163fddfb493019bd3c9db1
CARLOS EDUARDO TEIXEIRA GUIA <021.898687@senaicimatec.edu.br>
2024-05-08 14:02:29 -0300

    backup

--- src/components/Info.js
+++ src/components/Info.js
@@ -13,7 +13,7 @@ function Info(){
             <img class="logo-bottom" src={imgLogo} alt="Logo"></img>
             <h1 class="text-body-emphasis">Saiba direito dos seus direitos!</h1>
             <p class="col-lg-8 mx-auto fs-5 text-muted">
-              O Código de Defesa do Consumidor estabelece que produtos e serviços devem oferecer informação adequada, com especificação correta de quantidade, características, composição, qualidade, tributos incidentes e preço, bem como sobre os riscos que apresentem.
+              O Código de Defesa do Consumidor estabelece que produtos e serviços devem oferecer informação adequada, com especificação correta de quantidade, característicassgit, composição, qualidade, tributos incidentes e preço, bem como sobre os riscos que apresentem.
             </p>
             <div class="d-inline-flex gap-2 mb-5">
               <a href="Login e cadastro/login.html" class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
